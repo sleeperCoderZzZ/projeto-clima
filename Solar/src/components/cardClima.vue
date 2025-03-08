e0c9f9c874caf959f7330ec5da6ac467ed1066bd
Cabral <jp660595@gmail.com>
2025-03-08 14:15:15 -0300

    refactor: replace HelloWorld and TheWelcome components with CardClima; update styles and add gradient background

---
+++ Solar/src/components/cardClima.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card-filho">
+        <div class="card-header">
+            <h2>Clima Santo antonio da Platina</h2>
+        </div>
+        <div class="card-body">
+            <div class="card-body__content">
+                <div class="card-body__content--info">
+                    <p>Temperatura: 25°C</p>
+                    <p>Estado: Soleado</p>
+                    <p>Humedad: 50%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+    
+</script>
+
+<link rel="stylesheet" href="/Solar/src/assets/cardClima.css">
